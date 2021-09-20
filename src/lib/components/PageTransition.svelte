04bd5274391f2d5f07491daea7d0483b9ad01533
akaregi <verdigris@azure.icu>
2021-09-21 01:22:29 +0900

    design: Added page transition component
    Signed-off-by: akaregi <verdigris@azure.icu>

---
+++ src/lib/components/PageTransition.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { fly } from "svelte/transition";
+    export let refresh = "";
+</script>
+
+{#key refresh}
+    <div
+        in:fly={{ x: -10, duration: 250, delay: 250 }}
+        out:fly={{ x: 10, duration: 250 }}
+    >
+        <slot />
+    </div>
+{/key}
