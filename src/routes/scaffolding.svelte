e5a3754aa790b704bdeccd16e17e50efbbf158a0
akaregi <verdigris@azure.icu>
2021-09-20 15:12:10 +0900

    other: fixed title
    Signed-off-by: akaregi <verdigris@azure.icu>

--- src/routes/scaffolding.svelte
+++ src/routes/scaffolding.svelte
@@ -7,7 +7,7 @@
 </svelte:head>
 
 <section>
-    <Header title="Sample" />
+    <Header title="Scaffold" />
     <h2>HTTP 999 I'm going to.</h2>
     <p>
         I'm going to create this page, but now here's nothing but the white
